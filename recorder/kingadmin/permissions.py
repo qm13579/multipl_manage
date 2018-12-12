8cc7e7040ca2419337611d7b2fddaf79d2b78f16
fz <fz@qq.com>
2018-12-12 13:16:44 +0800

    commit per/check_per.py

---
+++ recorder/kingadmin/permissions.py
@@ -0,0 +1,7 @@
+
+
+def per():
+    '''判断是否有权限'''
+
+def check_permissions():
+    '''检查权限'''
--- recorder/kingadmin/urls.py
+++ recorder/kingadmin/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from kingadmin import views
 
 urlpatterns = [
-    url(r'^$',views.home),
+    url(r'^$',views.home,name='index'),
     url(r'^(\w+)/(\w+)$',views.app_table,name='appname'),
     url(r'^(\w+)/(\w+)/(\d+)/change$',views.table_change,name='change'),
     url(r'^(\w+)/(\w+)/(\d+)/delete$',views.table_delete,name='delete'),
