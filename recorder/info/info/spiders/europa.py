d74ab1bcec62147741b9ed3f01038b1bdd31744e
fengzhi <37413624@qq.com>
2018-12-17 23:53:56 +0800

    cpmmit scrapy1.2

--- recorder/info/info/spiders/europa.py
+++ recorder/info/info/spiders/europa.py
@@ -12,28 +12,34 @@ class EuropaSpider(scrapy.Spider):
     start_urls = ['https://www.ecb.europa.eu/']
     url_set=set()
     def parse(self, response):
-        content = Selector(response=response).xpath('//a')
-        for select_uul in content:
-            url = select_uul.xpath('.//@href').extract_first()
-            text = select_uul.xpath('.//text()').extract_first()
-            if not url:continue
-            if not text:continue
-            if re.findall('.*pdf.*',url):
-                if text.strip():
-                    if self.md5(url) in self.url_set:
-                        continue
-                    else:
-                        self.url_set.add(self.md5(url))
-                        print(self.start_urls[0]+url)
+
+        self.pdf_info(response=response)
+
         # 持久化url
-        # content = Selector(response=response).xpath('//a/@href').extract()
-        # for url in content:
-        #     if len(url) < 2 :continue
-        #     elif  not url.startswith('/') : continue
-        #     elif url[1] == '/':continue
-        #     print(url)
+        content = Selector(response=response).xpath('//a/@href').extract()
+        for url in content:
+            if len(url) < 2 :continue
+            elif  not url.startswith('/') : continue
+            elif url[1] == '/':continue
+            url=self.start_urls[0]+url
+            yield Request(url=url,callback=self.parse)
     def md5(self,url):
         import hashlib
         obj=hashlib.md5()
         obj.update(bytes(url,encoding='utf-8'))
         return obj.hexdigest()
+    def pdf_info(self,response):
+        '''获取pdf信息并持久化'''
+        content = Selector(response=response).xpath('//a')
+        for select_uul in content:
+            url = select_uul.xpath('.//@href').extract_first()
+            text = select_uul.xpath('.//text()').extract_first()
+            if not url: continue
+            if not text: continue
+            if re.findall('.*pdf.*', url):
+                if text.strip():
+                    if self.md5(url) in self.url_set:
+                        continue
+                    else:
+                        self.url_set.add(self.md5(url))
+                        print(self.start_urls[0] + url)
