47ecf6a97b21766861a3181991b40def8a2039f4
fengzhi <37413624@qq.com>
2019-01-01 22:57:03 +0800

    对监测爬虫进行增加链接的判断

--- recorder/info/monit/monit/spiders/eur.py
+++ recorder/info/monit/monit/spiders/eur.py
@@ -3,10 +3,13 @@ import scrapy
 from scrapy.selector import Selector,HtmlXPathSelector
 from scrapy.http import Request
 import re
-import sys,io
+import hashlib
 from ..items import EuropaItem
 from info.models import WebInfo,UrlInfo
 from urllib import parse
+import sys, io,json
+import requests
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
 
 class EurSpider(scrapy.Spider):
     name = 'eur'
@@ -18,7 +21,6 @@ class EurSpider(scrapy.Spider):
     for obj in WebInfo.objects.all():
         url_set.add(obj.md5)
     def parse(self, response):
-        print('--------->',response.url)
         item_list = self.pdf_info(response=response)
         # for item_dict in item_list:
         #     item_obj = EuropaItem(
@@ -36,7 +38,6 @@ class EurSpider(scrapy.Spider):
             url=parse.urljoin(response.url,url)
             yield Request(url=url,callback=self.parse)
     def md5(self, url):
-        import hashlib
         obj = hashlib.md5()
         obj.update(bytes(url, encoding='utf-8'))
         return obj.hexdigest()
@@ -45,6 +46,9 @@ class EurSpider(scrapy.Spider):
         '''获取pdf信息并持久化'''
         content = Selector(response=response).xpath('//a')
         item_list = []
+        for i in self.start_urls:
+            if re.findall('.*%s.*'%i,response.url):
+                base_url=i
         for select_uul in content:
             url = select_uul.xpath('.//@href').extract_first()
             text = select_uul.xpath('.//text()').extract_first()
@@ -56,10 +60,39 @@ class EurSpider(scrapy.Spider):
                         continue
                     else:
                         self.url_set.add(self.md5(url))
-                        href = self.start_urls[0] + url
+                        href = base_url + url
                         item_dict = {}
                         item_dict['title'] = text
                         item_dict['href'] = href
                         item_dict['md5'] = self.md5(url)
                         item_list.append(item_dict)
+                        # print(item_dict)
         return item_list
+
+    def translation(self,content):
+        url = "http://api.fanyi.baidu.com/api/trans/vip/translate"
+        appid = '20170602000050620'
+        secret = 'S05sDZgY7JdamCHUGFFY'
+        salt = '12345678'
+        hl = hashlib.md5()
+        text = appid + content + salt + secret
+        hl.update(text.encode('utf-8'))
+        sign = hl.hexdigest()
+        param = {
+            'q': content,
+            'from': 'en',
+            'to': 'zh',
+            'appid': appid,
+            'salt': salt,
+            'sign': sign
+        }
+        session = requests.session()
+        session.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        response = session.get(url, params=param)
+        rsp = json.loads(response.text)
+        return rsp['trans_result'][0]['dst']
+    def participle(self,content):
+        '''采用结巴精确模式进行分词'''
+        import jieba
+        seg_list=jieba.cut(content)
+        return seg_list
