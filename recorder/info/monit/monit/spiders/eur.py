5499efa40e1a15e328313daac1b91390893b1718
fz@jzq <29482004@qq.com>
2019-02-26 14:59:11 +0800

    提交一次临时分支

--- recorder/info/monit/monit/spiders/eur.py
+++ recorder/info/monit/monit/spiders/eur.py
@@ -5,24 +5,21 @@ from scrapy.http import Request
 import re
 import hashlib
 from ..items import EuropaItem
-<<<<<<< HEAD
 from info.models import WebInfo,UrlInfo
 from urllib import parse
 import sys, io,json
 import requests
 import jieba.analyse
 
-=======
 import sys, io
 from urllib import parse
 from info.models import WebInfo
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
+
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
 
 class EurSpider(scrapy.Spider):
     name = 'eur'
     allowed_domains = ['ecb.europa.eu']
-<<<<<<< HEAD
     start_urls = []
     for obj in UrlInfo.objects.all():
         start_urls.append(obj.base_url)
@@ -30,20 +27,18 @@ class EurSpider(scrapy.Spider):
     for obj in WebInfo.objects.all():
         url_set.add(obj.md5)
 
-=======
     # start_urls = ['https://www.ecb.europa.eu/','http://www.treasury.gov.za']
     start_urls = ['https://www.ecb.europa.eu/','http://www.treasury.gov.za']
     url_set=set()
     for i in WebInfo.objects.all():
         url_set.add(i)
     item_list = []
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
+
     def parse(self, response):
         # print(response.url)
         item_list = self.pdf_info(response=response)
         # print('---->')
         for item_dict in item_list:
-<<<<<<< HEAD
             item_obj = EuropaItem(
                 title=item_dict['title'],
                 url=item_dict['href'],
@@ -63,7 +58,6 @@ class EurSpider(scrapy.Spider):
             elif url[1] == '/':continue
             url=parse.urljoin(response.url,url)
             yield Request(url=url,callback=self.parse)
-=======
             # 数据持久化
             if  item_dict['title'] =='ENGLISH' : continue
             # print(item_dict)
@@ -83,7 +77,6 @@ class EurSpider(scrapy.Spider):
             #     url=self.start_urls[0]+url
             #     # yield Request(url=url,callback=self.parse)
             #     yield Request(url=parse.urljoin(response.url,url),callback=self.parse)
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
 
     def md5(self, url):
         obj = hashlib.md5()
@@ -93,13 +86,10 @@ class EurSpider(scrapy.Spider):
     def pdf_info(self, response):
         '''获取pdf信息并持久化'''
         content = Selector(response=response).xpath('//a')
-<<<<<<< HEAD
         item_list = []
         for i in self.start_urls:
             if re.findall('.*%s.*'%i,response.url):
                 base_url=i
-=======
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
         for select_uul in content:
             url = select_uul.xpath('.//@href').extract_first()
             text = select_uul.xpath('.//text()').extract_first()
@@ -111,17 +101,13 @@ class EurSpider(scrapy.Spider):
                         continue
                     else:
                         self.url_set.add(self.md5(url))
-<<<<<<< HEAD
                         href = base_url + url
-=======
                         # href = response.url + url
                         href = self.match_url(response.url,url)
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
                         item_dict = {}
                         item_dict['title'] = text
                         item_dict['href'] = href
                         item_dict['md5'] = self.md5(url)
-<<<<<<< HEAD
                         item_dict['keyword'] = self.participle(text)
                         item_list.append(item_dict)
                         # print(item_dict)
@@ -162,8 +148,7 @@ class EurSpider(scrapy.Spider):
             for i in range(5-len(msg_list)):
                 msg_list.append( '')
         return msg_list
-=======
-                        self.item_list.append(item_dict)
+                self.item_list.append(item_dict)
         return self.item_list
 
     def match_url(self,response_url,url):
@@ -171,4 +156,3 @@ class EurSpider(scrapy.Spider):
         for base_url in self.start_urls:
             if re.findall('.*%s.*'%base_url,response_url):
                 return base_url+url
->>>>>>> f95d10c5798e9a69097096a2f15114fa10299c19
